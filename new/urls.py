452f7a981208ee11ccabf745317f95f2affbc796
Bagurgl <danek_penek@mail.ru>
2022-10-06 23:28:37 +0300

    Сделал ссылки правильнее(name)
    поменял шаблон

--- new/urls.py
+++ new/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),
-    path('home/', index1),
-    path('category/<int:category_news_id>/', get_category),
+    path('', index1),
+    path('category/<int:category_news_id>/', get_category, name='category'),
 ]
 
