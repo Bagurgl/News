0ae6d5c50ba5fcfe8d334e7d22937fc2ddaf855a
Bagurgl <danek_penek@mail.ru>
2022-10-07 15:53:08 +0300

    Ссылки на читать далее...

--- new/urls.py
+++ new/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),
-    path('home/', index1),
-    path('category/<int:category_news_id>/', get_category),
+    path('', index1, name='home'),
+    path('category/<int:category_news_id>/', get_category, name='category'),
+    path('news/<int:new_id>', get_new, name='new'),
 ]
-
--- new/views.py
+++ new/views.py
@@ -3,20 +3,11 @@ from django.shortcuts import render, HttpResponse
 from .models import *
 
 
-def index(request):
-    news = New.objects.all()  # Сортировка: object.order_by(-name)         Филтр: object.filter(id=2)
-    context = {
-        'news__': news,
-        'title': 'Новости ВТБ',
-    }
-    return render(request, 'new/index.html', context)
-
-
 def index1(request):
     news = New.objects.all()  # Сортировка: object.order_by(-name)         Филтр: object.filter(id=2)
     cat = Category_News.objects.all()
     context = {
-        'news__': news,
+        'news': news,
         'title': 'Новости ВТБ',
         'category': cat,
     }
@@ -29,3 +20,10 @@ def get_category(request, category_news_id):
     category = Category_News.objects.get(pk=category_news_id)
     return render(request, 'new/category.html', {'news': news, 'cats': cats,
                                                  'category': category})
+
+
+def get_new(request, new_id):
+    new = New.objects.filter(pk=new_id)
+    cats = Category_News.objects.all()
+    return render(request, 'new/get_new.html', {'new': new, 'cats': cats})
+
